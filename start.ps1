971969c70d2978266affbd3e13ec4fddbc478b9e
roothefishy <ryandesend@gmail.com>
2026-04-22 18:44:11 +0200

    Update start.ps1

--- start.ps1
+++ start.ps1
@@ -1,4 +1,4 @@
-Start-Process cmd -Verb RunAs -ArgumentList '/k powershell set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/roothefishy/SS/MCTools.ps1)'
+Start-Process cmd -Verb RunAs -ArgumentList '/k powershell set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/roothefishy/SS/main/MCTools.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/praiselily/lilith-ps/refs/heads/main/Services.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/zedoonvm1/powershell-scripts/refs/heads/main/DoomsDayDetector.ps1)'
 Start-Process cmd -Verb RunAs -ArgumentList '/k powershell Set-ExecutionPolicy -Scope Process Bypass; Invoke-Expression (Invoke-RestMethod https://raw.githubusercontent.com/Enr1c0o/Powershell-Scripts/refs/heads/main/Alt-Detector.ps1)'
